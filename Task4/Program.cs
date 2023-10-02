d36b334697c6713442b1c4acf219f33135682941
Shakirov Nikita <shakirnik16@gmail.com>
2023-10-02 19:21:35 +0300

    акончил задачу 4

---
+++ Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿int numberA = 2, 
+    numberB = 3,
+    numberC = 7,
+    max = 0;
+
+if (numberA > max)
+{
+    max = numberA;
+}
+if (numberB > max)
+{
+    max = numberB;
+}
+if (numberC > max)
+{
+    max = numberC;
+}
+
+Console.Write("Самое большое число: ");
+Console.WriteLine(max);
