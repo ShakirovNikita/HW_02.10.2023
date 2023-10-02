4f2d58e429119cc8634b806b6f88bcb299183ceb
Shakirov Nikita <shakirnik16@gmail.com>
2023-10-02 19:33:56 +0300

    закончил задачу 6

---
+++ Task6/Program.cs
@@ -0,0 +1,11 @@
+﻿string number = Console.ReadLine()!;
+int result = Convert.ToInt32(number);
+
+if (result % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else 
+{
+    Console.WriteLine("Число нечетное");
+}
