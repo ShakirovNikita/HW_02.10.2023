ea42a2495b2b540fc2d773f04e4ab3f69be1c8dc
Shakirov Nikita <shakirnik16@gmail.com>
2023-10-02 19:02:54 +0300

    Закончил задачу 2

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿int numberA = 5;
+int numberB = 7;
+
+if (numberA > numberB)
+{
+    Console.Write("Самое большое число: ");
+    Console.WriteLine(numberA);
+}
+else 
+{
+    Console.Write("Самое большое число: ");
+    Console.WriteLine(numberB);
+}
