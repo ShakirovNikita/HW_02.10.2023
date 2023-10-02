3f436cd36c4d85dd44644852e12ac6231a761470
Shakirov Nikita <shakirnik16@gmail.com>
2023-10-02 19:43:27 +0300

    задача 8

---
+++ Task8/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = 2;
+int count = 1;
+int bufer = 1;
+
+
+while (count < n+1)
+{
+    if (count%2==0)
+    {
+        bufer = count;
+        Console.WriteLine(bufer);
+    }
+    count++;
+}
